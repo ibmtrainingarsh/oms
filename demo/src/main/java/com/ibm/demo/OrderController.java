d574fd39c4c245897a50133b8df58bc92c367cdf
unknown <ibmjavanh14@iiht.tech>
2021-03-25 12:36:26 +0530

    Added Services and validation

--- demo/src/main/java/com/ibm/demo/OrderController.java
+++ demo/src/main/java/com/ibm/demo/OrderController.java
@@ -1,34 +1,49 @@
 package com.ibm.demo;
 
-import org.springframework.http.HttpRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibm.demo.entity.Order;
+import com.ibm.demo.service.OrderService;
+
 @RestController
-public class OrderController {
+public class OrderController { //frontend
+	@Autowired
+	OrderService orderService; //DI
 	@PostMapping("/order")
-	String createOrder() {
-		return "success";
+	String createOrder(@RequestBody @Valid Order order, BindingResult bindingResult){
+		if(bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("Something went wrong");
+		}
+		System.out.println(order);
+		return orderService.createOrder(order);
 	}
+
 	@GetMapping("/order")
 	String getOrder() {
 		return "order created";
 	}
+
 	@PutMapping("/order/{id}")
-	String updateOrder(@PathVariable("id") int orderId ) {
+	String updateOrder(@PathVariable("id") int orderId) {
 		System.out.println(orderId);
 		return "order updated";
 	}
+
 	@DeleteMapping("/order/{id}")
-	String deleteOrder(@PathVariable("id") int orderId, HttpRequest httpRequest ) {
+	String deleteOrder(@PathVariable("id") int orderId) {
 		System.out.println(orderId);
-		System.out.println(httpRequest.getHeaders());
+//		System.out.println(httpRequest.getHeaders());
 		return "order deleted";
 	}
-
 
 }
