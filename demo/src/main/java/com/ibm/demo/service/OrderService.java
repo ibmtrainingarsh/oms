451c6536cdd1e0a6686081c4942d7aa9efbff38d
unknown <ibmjavanh14@iiht.tech>
2021-03-26 12:25:47 +0530

    implemented GET and Update

--- demo/src/main/java/com/ibm/demo/service/OrderService.java
+++ demo/src/main/java/com/ibm/demo/service/OrderService.java
@@ -2,6 +2,7 @@ package com.ibm.demo.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,16 +18,17 @@ public class OrderService {
 		return savedOrder.getId();
 	}
 	public List<Order> getOrders() {
-		return new ArrayList<Order>();
+		return orderRepository.findAll();
 	}
 	
-	public void updateOrder(int orderId) {
+	public void updateOrder(Order order) {
+		orderRepository.save(order);
 		
 	}
 	public void deleteOrder(int orderId) {
 		System.out.println ("order deleted");
 	}
-	public Order getOrder(int orderId) {
-		return new Order();
+	public Optional<Order> getOrder(String orderId) {
+		return orderRepository.findById(orderId);
 	}
 }
